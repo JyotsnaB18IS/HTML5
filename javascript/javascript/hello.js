571077ff3fa1bbf01eabc7d6c406c88c594f25b5
JyotsnaB18IS <jyotsnab.18is@saividya.ac.in>
2022-07-01 18:54:11 +0530

    Html version 1.0

---
+++ javascript/javascript/hello.js
@@ -0,0 +1,13 @@
+        document.writeln("hello world from javascript");
+        document.writeln("<h1>hello world from javascript");
+
+        var a,b, sum;
+       var name=prompt("Enter your name");
+        a=parseInt(prompt("Enter your first num:","Enter a number"));
+        b=parseInt(prompt("Enter your second num:","Enter a number"));
+        sum=a+b;
+        
+        document.write("<br>"+"Hello "+name+"<br>");
+        document.write("Addition of 2 nums is: "+sum);
+        alert("Thankyou");
+       
